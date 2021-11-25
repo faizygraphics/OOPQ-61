62104bf7d763de017053be94fdcac1195d266fa1
faizygraphics <fazitanveer2@gmail.com>
2021-11-25 13:59:52 +0500

    OOP

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+        // (a)
+      int  a, b, c, d, e;
+     a=10, b=20, c=15, d=8, e=40;
+    // Let n=equation 
+        {
+          int n = (a+b/(c-5))/((d+7)/(e-37)%3);
+          Console.WriteLine(n);
+          Console.ReadLine();
+        }
+        {
+          // (b)
+          int p = a+b/c-5/d+7/e-37%3;
+          console.WriteLine(p);
+          console.ReadLine();
+        }
+        {
+          // (c)
+          int q = a* (b*b) - (c*b) + d;
+          console. WriteLine(q);
+          console.ReadLine();
+        }
+      
+     }
+  }
+}
